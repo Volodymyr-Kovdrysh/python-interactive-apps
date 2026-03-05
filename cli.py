360a3d2453265b17348f5b01fb1fc495185dc232
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2026-03-05 11:49:32 +0200

    Крок 1

--- cli.py
+++ cli.py
@@ -7,76 +7,79 @@ os.makedirs("data", exist_ok=True)
 todos_path = os.path.join("data", "todos.txt")
 
 if not os.path.exists(todos_path):
-    with open(todos_path, "w") as f:
+    with open(todos_path, "w", encoding='utf-8') as f:
         pass
 
-now = time.strftime("%b %d, %Y %H:%M:%S")
-print(now)
-while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.strip()
 
 
-    if user_action.lower().startswith('add'):
-        todo = user_action[4:] + "\n"
+def repl():
+    now = time.strftime("%b %d, %Y %H:%M:%S")
+    print(now)
+    while True:
+        user_action = input("Type add, show, edit, complete or exit: ")
+        user_action = user_action.strip()
 
-        todos = functions.get_todos()
 
-        todos.append(todo)
+        if user_action.lower().startswith('add'):
+            todo = user_action[4:] + "\n"
 
-        functions.write_todos(todos)
-
-    elif user_action.lower().startswith('show'):
-        todos = functions.get_todos()
-
-        for index, item in enumerate(todos):
-            row = f"{index+1} -- {item.strip('\n')}"
-            print(row)
-    elif user_action.lower().startswith('edit'):
-        try:
-            number = int(user_action[5:])
-
-            number = number - 1
-            if number < 0:
-                raise IndexError
             todos = functions.get_todos()
 
-            new_todo = input("Enter new todo: ")
-            todos[number] = new_todo + '\n'
-
-            functions.write_todos(todos)
-        except ValueError:
-            print("Ваша команда не зовсім зрозуміла")
-            continue
-        except IndexError:
-            print("Не вірний номер тудушки")
-            continue
-
-
-    elif user_action.lower().startswith('complete'):
-        try:
-            number = int(user_action[9:])
-            number = number - 1
-            if number < 0:
-                raise IndexError
-            todos = functions.get_todos()
-
-            completed_todo = todos.pop(number)
+            todos.append(todo)
 
             functions.write_todos(todos)
 
-            message = f"\tТудушка \"{completed_todo.strip('\n')}\" була успішно виконана!"
-            print(message)
-        except IndexError:
-            print("Не вірний номер тудушки")
-            continue
+        elif user_action.lower().startswith('show'):
+            todos = functions.get_todos()
 
-    elif user_action.lower().startswith('exit'):
-        break
-    else:
-        print('invalid input')
+            for index, item in enumerate(todos):
+                row = f"{index+1} -- {item.strip('\n')}"
+                print(row)
+        elif user_action.lower().startswith('edit'):
+            try:
+                number = int(user_action[5:])
 
-    print( ' Успішне виконня команди')
+                number = number - 1
+                if number < 0:
+                    raise IndexError
+                todos = functions.get_todos()
+
+                new_todo = input("Enter new todo: ")
+                todos[number] = new_todo + '\n'
+
+                functions.write_todos(todos)
+            except ValueError:
+                print("Ваша команда не зовсім зрозуміла")
+                continue
+            except IndexError:
+                print("Не вірний номер тудушки")
+                continue
+
+
+        elif user_action.lower().startswith('complete'):
+            try:
+                number = int(user_action[9:])
+                number = number - 1
+                if number < 0:
+                    raise IndexError
+                todos = functions.get_todos()
+
+                completed_todo = todos.pop(number)
+
+                functions.write_todos(todos)
+
+                message = f"\tТудушка \"{completed_todo.strip('\n')}\" була успішно виконана!"
+                print(message)
+            except IndexError:
+                print("Не вірний номер тудушки")
+                continue
+
+        elif user_action.lower().startswith('exit'):
+            break
+        else:
+            print('invalid input')
+
+        print( ' Успішне виконня команди')
 
 
 
@@ -84,4 +87,13 @@ while True:
 
 
 
-print("Babay!")
+    print("Babay!")
+
+
+def main():
+    repl()
+
+
+
+if __name__ == '__main__':
+    main()
